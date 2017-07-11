bd215f22dbeca31c634d060c768dd2aa1dbccfbf
Ninec23 <nikolina.cindric2@gmail.com>
2017-07-11 11:24:00 +0200

    header.php
    Promijena svega

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color:#66B2FF;">
+<div class="container-fluid">
+  <header style="background-color:#FFFFFF;">
+  <ul class="nav nav-tabs">
+  <li class="active"><a href="index.html"><img src="https://s-media-cache-ak0.pinimg.com/600x315/6a/ba/28/6aba28906bb783874e4d06697ff21c79.jpg" style="height:50px; weight:50px;">Maniva Putovanja</a></li>
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="putovanja.php">Putovanja
+    <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+      <li><a href="domaca.php">Domaća putovanja</a></li>
+      <li><a href="inozemna.php">Inozemna putovanja</a></li>
+    </ul>
+  </li>
+  <li><a href="ostalo.php">Posebne ponude</a></li>
+  <li><a href="kontakt.php">Kontakt</a></li>
+</ul>
+    </header>
